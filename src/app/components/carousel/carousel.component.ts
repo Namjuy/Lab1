c734ef09a0e67a0896a5fb9c1600ee12979e5dbd
Namjuy <najumy1807.2k@gmail.com>
2023-12-22 09:05:47 +0700

    update final v2

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -26,19 +26,19 @@ export class CarouselComponent implements OnInit {
     {
       title: 'NĂNG LỰC CÔNG NGHỆ',
       detail:
-        'BA GPS tự hào mang đến cho ngành giao thông vận tải các sản phẩm ứng dụng các công nghệ lõi',
+        'BA GPS tự hào mang đến cho ngành giao thông vận tải các sản phẩm ứng dụng các công nghệ lõi.',
       image: 'https://bagps.vn/public/media//art_2801.jpg',
     },
     {
-      title: 'HỖ TRỢ ĐỊNH VỊ PHƯƠNG TIỆN NGOÀI KHƠI',
+      title: 'HỖ TRỢ CẢM BIẾN NHIÊN LIỆU',
       detail:
-        'GPS định vị toạ độ của phương tiện đảm bảo sự an toàn của người dân',
+        'Tối ưu chi phí thông qua việc lắp đặt cảm biến nhiên liệu tích hợp định vị trên xe.',
       image: 'https://i.ytimg.com/vi/_sSd4OVYVCk/maxresdefault.jpg',
     },
     {
-      title: 'HỖ TRỢ ĐỊNH VỊ PHƯƠNG TIỆN NGOÀI KHƠI',
+      title: 'HỖ TRỢ ĐỊNH VỊ PHƯƠNG TIỆN',
       detail:
-        'GPS định vị toạ độ của phương tiện đảm bảo sự an toàn của người dân',
+        'GPS định vị toạ độ của phương tiện đảm bảo sự an toàn của người dân.',
       image:
         'https://bagps.vn/public/media//tai_li%E1%BB%87u/4_%C6%AFu_vi%E1%BB%86t_giup_thong_thai_l%E1%BB%B0a_ch%E1%BB%8Cn_camera_ngh%E1%BB%8A_%C4%90%E1%BB%8Anh_10_ti%E1%BA%BEt_ki%E1%BB%86m_7_tri%E1%BB%86u_vn%C4%90.png',
     },
