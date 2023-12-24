2e09e5236a99ac205515137242cbab7d21cc133b
Namjuy <najumy1807.2k@gmail.com>
2023-12-24 22:08:26 +0700

    update code

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  title: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,38 +16,56 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.menuItems = [...this.vietnameseMenuItems]
+  }
 
+  // -----Initialize selected language ------------
+  selectedLanguage = 'vi';
 
-  //Initialize the navbar menu item array
-  menuItems = [
-    {
-      title: 'Trang chủ',
-      link: 'https://bagps.vn/',
-    },
+  // ---------Initialize menu item---------------
+  menuItems: MenuItem[] = [];
+
+  // ------------Initialize vietnamese menu item-------------
+  vietnameseMenuItems: MenuItem[] = [
+    { title: 'Trang chủ', link: 'https://bagps.vn/' },
     {
       title: 'Sản phẩm dịch vụ',
       link: 'https://bagps.vn/san-pham-va-giai-phap',
     },
-    {
-      title: 'Tin tức',
-      link: 'https://bagps.vn/tin-tuc-c10',
-    },
+    { title: 'Tin tức', link: 'https://bagps.vn/tin-tuc-c10' },
     {
       title: 'Đóng phí',
       link: 'https://bagps.vn/huong-dan-dong-phi-dich-vu-ba-gps-d610',
     },
-    {
-      title: 'Hướng dẫn',
-      link: 'https://badoc.bagroup.vn/x/SAGhBg',
-    },
-    {
-      title: 'Mạng lưới',
-      link: 'https://bagps.vn/mang-luoi',
-    },
-    {
-      title: 'Về chúng tôi',
-      link: 'https://bagps.vn/gioi-thieu/',
-    },
+    { title: 'Hướng dẫn', link: 'https://badoc.bagroup.vn/x/SAGhBg' },
+    { title: 'Mạng lưới', link: 'https://bagps.vn/mang-luoi' },
+    { title: 'Về chúng tôi', link: 'https://bagps.vn/gioi-thieu/' },
   ];
+
+  // ------------English vietnamese menu item-------------
+  englishMenuItems: MenuItem[] = [
+    { title: 'Home', link: 'https://bagps.vn/' },
+    {
+      title: 'Products and Services',
+      link: 'https://bagps.vn/san-pham-va-giai-phap',
+    },
+    { title: 'News', link: 'https://bagps.vn/tin-tuc-c10' },
+    {
+      title: 'Pay Fees',
+      link: 'https://bagps.vn/huong-dan-dong-phi-dich-vu-ba-gps-d610',
+    },
+    { title: 'Guides', link: 'https://badoc.bagroup.vn/x/SAGhBg' },
+    { title: 'Network', link: 'https://bagps.vn/mang-luoi' },
+    { title: 'About Us', link: 'https://bagps.vn/gioi-thieu/' },
+  ];
+
+  // -----------------Create method change language when choose dropdown-----
+  changeLanguage(language: string) {
+    this.selectedLanguage = language;
+    this.menuItems =
+      language === 'en'
+        ? [...this.englishMenuItems]
+        : [...this.vietnameseMenuItems];
+  }
 }
